e55c75d40ad22282116e587dab3eaedb158f8979
Michael Lee <mlee@MacBook-Pro.local>
2025-04-29 23:04:35 -0400

    finished db setup

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	_ "github.com/lib/pq"
 )
 type APIServer struct {
 	listenAddr string
@@ -14,10 +17,37 @@ func NewAPIServer(listenAddr string) *APIServer {
 	}
 }
 
+func (s *APIServer) DataBaseHandler() {
+	const (
+		host = "localhost"
+		port = 5432
+		password = ""
+		db_name = "sports-management-platform-db"
+	)
+
+	// Connect to the database
+	pqsqlconn := fmt.Sprintf("host=%s port=%d user=mlee password=%s dbname=%s sslmode=disable", host, port, password, db_name)
+
+	db, err := sql.Open("postgres", pqsqlconn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	// Verify the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Unable to ping the database: %v", err)
+	}
+
+	log.Println("Successfully connected to the database!")
+}
+
 func (s *APIServer) Run() {
 
 	router := http.NewServeMux();
-
+	s.DataBaseHandler();
 	log.Println("JSON API server running on port: ", s.listenAddr)
 	err := http.ListenAndServe(s.listenAddr, router)
 	if err != nil {
